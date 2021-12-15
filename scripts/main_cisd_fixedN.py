4251df49e4ad66bca5841cab814c07cc2d59084c
Gaurav Harsha <gauravharsha05@gmail.com>
2021-12-15 17:51:03 -0600

    flakify main_cisd_fixedN.py

--- scripts/main_cisd_fixedN.py
+++ scripts/main_cisd_fixedN.py
@@ -1,5 +1,9 @@
-import numpy as np, time, sys
-from tfdcisd import *
+import numpy as np
+import time
+import sys
+from h5py import File
+from scipy.special import comb
+from tfdcisd import Evolution, eval_energy, eval_number
 
 #
 # GLOBAL VARIABLES
@@ -12,11 +16,12 @@ len_t2 = 0
 
 #
 # MAIN
-# 
+#
 
 def main():
+
     start_time = time.time()
-    
+
     #################################################################
     #                       READ INPUT                              #
     #       Get the data first - including the attributes           #
@@ -34,12 +39,7 @@ def main():
 
     # Extract the parameters
     nso = evol.nso
-    n_elec = evol.n_elec
-    beta_f = evol.beta_f
     beta_pts = evol.beta_pts
-    beta_step = evol.beta_step
-    ntol = evol.ntol
-    deqtol = evol.deqtol
     e_nuc = evol.e_nuc
     fug = evol.fug
 
@@ -47,10 +47,10 @@ def main():
     eigs = evol.eigs
     h1 = evol.h1
     eri = evol.eri
-    
+
     input_time = time.time()
 
-    print('Number of Spin Orbitals:',nso)
+    print('Number of Spin Orbitals:', nso)
     print('--------------------------------------------------------------\n')
 
     #################################################################
@@ -62,23 +62,14 @@ def main():
     global len_t2
 
     len_t1 = int(nso**2)
-    len_t2 = int(comb(nso,2)**2)
-
-    # The T and Z vectors
-    t0 = 0.0
-    t1_vec = np.zeros(len_t1)
-    t2_vec = np.zeros(len_t2)
+    len_t2 = int(comb(nso, 2)**2)
 
     # CC and CI amps
     ci_amps = np.zeros(1 + len_t1 + len_t2)
 
     # HFB parameters
-    x = 1/np.sqrt( 1 + np.exp(-evol.beta_in*eigs + evol.alpha_in)*fug )
-    y = np.sqrt( 1 - x**2 )
-
-    # These are the actual t1 and t2 matrices
-    t1 = np.reshape(t1_vec,(nso,nso))
-    t2 = DecompressT2(t2_vec,nso)
+    x = 1/np.sqrt(1 + np.exp(-evol.beta_in*eigs + evol.alpha_in)*fug)
+    y = np.sqrt(1 - x**2)
 
     # Make arrays for energy, amplitudes, etc.
     beta_ci = np.zeros(evol.beta_pts)
@@ -89,21 +80,20 @@ def main():
     #################################################################
     #                   SETUP OUTPUT H5PY FILES                     #
     #################################################################
-    
-    output_fn = evol.fn.replace('int_files','output')
-    output_fn = output_fn.replace('_data.h5','_tfd_cisd.h5')
 
-    fout = h5py.File(output_fn,'w')
+    output_fn = evol.fn.replace('int_files', 'output')
+    output_fn = output_fn.replace('_data.h5', '_tfd_cisd.h5')
+
+    fout = File(output_fn, 'w')
 
     output_dsets = [
-        'beta','alpha','e_ci','n_ci'
+        'beta', 'alpha', 'e_ci', 'n_ci'
     ]
 
     evol.createh5(fout, output_dsets, beta_pts, evol.attrs)
 
     # the amplitudes datasets will have to be handled separately
-    fout.create_dataset('ci_amps',(beta_pts,1+len_t1+len_t2))
-
+    fout.create_dataset('ci_amps', (beta_pts, 1 + len_t1 + len_t2))
 
     #################################################################
     #                   INITIAL VALUE CHECK                         #
@@ -123,14 +113,14 @@ def main():
     evol.updateh5(vals, i_beta)
 
     # The CC and CI amplitudes at initial value are already ZEROS
-    
-    print('Input File Name = ',evol.fn)
-    print('Output File Name = ',output_fn)
+
+    print('Input File Name = ', evol.fn)
+    print('Output File Name = ', output_fn)
     print('--------------------------------------------------------------\n')
-    print('Initial Beta value = ',evol.beta_in)
-    print('Initial Alpha value = ',evol.alpha_in)
-    print('Internal energy at inf Temperature = ',e_ci[0])
-    print('Number of particles at inf Temperature = ',n_ci[0])
+    print('Initial Beta value = ', evol.beta_in)
+    print('Initial Alpha value = ', evol.alpha_in)
+    print('Internal energy at inf Temperature = ', e_ci[0])
+    print('Number of particles at inf Temperature = ', n_ci[0])
     print('--------------------------------------------------------------\n')
 
     init_time = time.time()
@@ -147,34 +137,38 @@ def main():
         # Do Beta Integration
         evol.DoBetaIntegration()
 
-        print('Beta = ',evol.beta_in)
+        print('Beta = ', evol.beta_in)
 
         # Do Alpha search and integration
         evol.BisectionAndAlphaIntegrate()
 
         # New HFB parameters
-        x = 1/np.sqrt( 1 + np.exp(-evol.beta_in*eigs + evol.alpha_in)*fug )
-        y = np.sqrt( 1 - x**2 )
+        x = 1/np.sqrt(1 + np.exp(-evol.beta_in*eigs + evol.alpha_in)*fug)
+        y = np.sqrt(1 - x**2)
 
         # Extract the amplitudes
         ci_amps = evol.ci_amps
 
         # Find Energy and Number
+        beta_ci[i_beta] = evol.beta_in
+        alpha_ci[i_beta] = evol.alpha_in
         e_ci[i_beta] = e_nuc + eval_energy(h1, eri, ci_amps, x, y)
         n_ci[i_beta] = eval_number(ci_amps, x, y)
 
         # Check by printing
-        print('Beta = ',evol.beta_in)
-        print('Alpha = ',evol.alpha_in)
-        print('Internal energy at new step = ',e_ci[i_beta])
-        print('Number of particles at new step = ',n_ci[i_beta])
-        print('--------------------------------------------------------------\n')
+        print('Beta = ', evol.beta_in)
+        print('Alpha = ', evol.alpha_in)
+        print('Internal energy at new step = ', e_ci[i_beta])
+        print('Number of particles at new step = ', n_ci[i_beta])
+        print(
+            '--------------------------------------------------------------\n'
+        )
 
         # Write data to the output files
         vals = [evol.beta_in, evol.alpha_in, e_ci[i_beta], n_ci[i_beta]]
         evol.updateh5(vals, i_beta)
         fout['ci_amps'][i_beta] = ci_amps
-        
+
     ode_time = time.time()
 
     #################################################################
@@ -201,6 +195,6 @@ def main():
 
     exit()
 
+
 if __name__ == '__main__':
     main()
-
